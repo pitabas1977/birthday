64d4837dc3bdfa90eb65eb8d3c00a7c6ad8bb5e5
Pitabas_S_Pradhan <200101120049@cutm.ac.in>
2021-12-12 07:27:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "SRAVANI" && b=="13-12-2002")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
